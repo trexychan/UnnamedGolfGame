b6bec5f500211abc2ebdffbb4c75574dd350fb53
skim73 <55114034+skim73@users.noreply.github.com>
2020-03-01 17:05:15 -0500

    Sound Volume Settings Done & Audio Folder added

---
+++ Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsScript : MonoBehaviour
+{
+    public GameObject BGM, SFX;
+    Slider musicSlider;
+    Slider sfxSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSlider = gameObject.GetComponentsInChildren<Slider>()[0];
+        sfxSlider = gameObject.GetComponentsInChildren<Slider>()[1];
+        if (musicSlider.name != "MusicSlider")
+        {
+            Slider temp = musicSlider;
+            musicSlider = sfxSlider;
+            sfxSlider = musicSlider;
+        }
+
+        BGM = GameObject.FindGameObjectWithTag("BGM");
+        SFX = GameObject.FindGameObjectWithTag("SFX");
+
+    }
+
+    public void changeMusicVolume()
+    {
+        BGM.GetComponent<AudioSource>().volume = musicSlider.value;
+    }
+
+    public void changeSFXVolume()
+    {
+        SFX.GetComponent<AudioSource>().volume = sfxSlider.value;
+    }
+}
